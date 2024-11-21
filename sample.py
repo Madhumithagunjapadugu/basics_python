c494aacdf257dceb54b7ca627ee5100f36f90786
Madhumithagunjapadugu <madhumithagunjapadugu@gmail.com>
2024-11-21 15:16:18 +0530

    Add files via upload
    my first python program

---
+++ sample.py
@@ -0,0 +1,8 @@
+# print("hello programmers")
+name = input()
+age = int(input())
+if(age>=18):
+    print("eligible to vote")
+else:
+    c=18-age
+    print(f"{name} have to come back after{c} years to vote")    
