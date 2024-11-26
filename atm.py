2948b48aeb3bff9e5864103ff55ac117eeab3840
Madhumithagunjapadugu <madhumithagunjapadugu@gmail.com>
2024-11-26 18:38:04 +0530

    Add files via upload
    atm also counting single notes

--- atm.py
+++ atm.py
@@ -2,13 +2,14 @@ print("*"*20,"ATM Meachine","*"*20)
 amount=int(input("Enter the amount: "))
 l=[500,200,100,50,20,10,5,2,1]
 c=0
+singlenotes=0
 for i in l:
     notes=amount//i
     c=c+notes
     print(f"{i} notes are --> {notes}")
     amount=amount-i*notes
     #or we can use amount=amount%i
-    if(c.count(notes)==1):
-        l2=[c]
-        print(sum(l2))
+    if(notes==1):
+        singlenotes+=1
+print(f" single notes are-->{singlenotes}")
 print(f" Minimum number of notes-->{c}")
